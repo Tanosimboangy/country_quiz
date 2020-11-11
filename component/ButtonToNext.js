f9543b18eb1b91041431bcd2913a475fe3e45cb0
Tanosimboangy <jacquit.aja@onja.org>
2020-11-11 08:01:36 +0300

    activating the next button

--- component/ButtonToNext.js
+++ component/ButtonToNext.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {Link} from "react-dom";
 
-function ButtonToNext({fetchingData, correct}) {
+function ButtonToNext({fetchingData, correct, count}) {
+    console.log(count);
     return (
         <>
             {correct ? 
-            <button onClick={fetchingData} >
+            <button onClick={fetchingData} className="btn_next" >
                 Next
             </button> 
             :
             <Link to="/restart">
-                <Restart fetchingData={fetchingData} />
+                <button onClick={fetchingData} className="btn_next" >
+                    Next
+                </button> 
             </Link>
             }
         </>
--- component/Diaplay.js
+++ component/Diaplay.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
  import ButtonToNext from "./ButtonToNext";
+ import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
- function Display({value, collectionItems, item1, item2, item3, item4, fetchingData}) {
+ function Display({
+     value, 
+     collectionItems, 
+     item1, 
+     fetchingData,
+    }) {
 
     const [correct, setCorrect] = useState(false);
     const [answered, setAnswered] = useState(false);
+    const [count, setCount] = useState(0);
 
     function handleClickButton(e) {
         const el = e.target;
@@ -12,60 +19,47 @@ import React, { useState } from "react";
         if ((el.dataset.value) === (value[item1].name)) {
             el.classList.add("correct");
             setCorrect(true);
+            setCount(prev => prev + 1);
         } else {
             el.classList.add("answered");
-            el.classList.add("correct");
-            setAnswered(false);
         }
     }
-    
     return (
-        <>
-            <div className="container">
-                <h2>{value[item1].capital} is the capital of?</h2>
-                {collectionItems.map(map => {
-                    return (
-                        <div key={value[map].name}>
-                            <button 
-                                value={value[map].name}
-                                data-value={value[map].name}
-                                onClick={handleClickButton}
-                                className="buttons">
-                                {value[map].name}<br />
-                            </button>
-                        </div>
-                    )
-                })}
-            </div>
-            {answered && 
-                <ButtonToNext 
-                fetchingData={fetchingData}
-                correct={correct}
-            />}
-        </>
+        <Router >
+            <Switch>
+                <Route exact path="/">
+                    <div className="container">
+                    <h2>{value[item1].capital} is the capital of?</h2>
+                    {collectionItems.map(map => {
+                        return (
+                            <div key={value[map].name}>
+                                <button 
+                                    value={value[map].name}
+                                    data-value={value[map].name}
+                                    onClick={handleClickButton}
+                                    className="buttons">
+                                    {value[map].name}<br />
+                                </button>
+                            </div>
+                            )
+                        })}
+                    </div>
+                </Route>
+                <Route path="/restart">
+                    <ButtonToNext 
+                    count={count}
+                    fetchingData={fetchingData}
+                    correct={correct} />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
 export default Display;
 
-
-{/* <div className="container">
-    <img style={{maxWidth:"100%"}} src={value[item1].flag} />
-    <h2>Which country does this flag belong to?</h2>
-    {collectionItems.map(map => {
-        return (
-            <div key={value[map].name}>
-                <button 
-                    value={value[map].name}
-                    onClick={(e) => console.log("I am here!")}
-                    className="buttons">
-                    {value[map].name}<br />
-                </button>
-        </div>
-        )
-    })}
-</div> */}
-
+{/* <img style={{maxWidth:"100%"}} src={value[item1].flag} />
+<h2>Which country does this flag belong to?</h2> */}
 
 // const API_CAPITAL = "https://restcountries.eu/rest/v2/capital/"
 // // Declaration UseState with Fetching the data for the capital
