b0053ed039c9df818706072b3b0e60f95ef0d034
Jacquit <jacquit.aja@onja.org>
2021-05-12 17:36:48 +0300

    fixing the svg

--- component/Display.js
+++ component/Display.js
@@ -1,55 +1,65 @@
-import React, { useState } from "react";
-import ButtonToNext from "./ButtonToNext";
-import Displaycontent from "./Displaycontent";
+import React, { useState } from 'react'
+import ButtonToNext from './ButtonToNext'
+import DisplayContent from './DisplayContent'
 
-function Display({ data, collectionItems, item1, fetchingData, count, setCount }) {
-    
-    // States for checking whether the user's answer is correct or not and for checking whether the user has anwered or not..
-    const [correct, setCorrect] = useState(false);
-    const [answered, setAnswered] = useState(false);
+function Display({
+  data,
+  collectionItems,
+  item1,
+  fetchingData,
+  count,
+  setCount,
+}) {
+  // States for checking whether the user's answer is correct or not and for checking whether the user has anwered or not..
+  const [correct, setCorrect] = useState(false)
+  const [answered, setAnswered] = useState(false)
 
-    // Function for setting the condition whether the user has answered or not yet for setting the condition whether the answer is correct or incorrect.
-    function handleClickButton(e) {
-        setAnswered(true);
-        if ((e.target.dataset.value) === (data[item1].name)) {
-            // If the condition is true, add this "true" class and set this setCorrect function into true.
-            e.target.classList.add("true_answer");
-            setCorrect(true);
-        } else if ((e.target.dataset.value) !== (data[item1].name)) {
-            // If this condition is true, add this "responded" class and set this setCorrect function into false.
-            e.target.classList.add("false_answer");
-            setCorrect(false);
-            // Finding the correct answer so that I can grab the button.
-            const rightButton = data[collectionItems.find(item => {
-                return data[item].name === data[item1].name;
-            })].name;
-            // Grab all of the buttons in order to find the correct one.
-            const buttons = Array.from(document.querySelectorAll(".buttons"));
-            // Finding the button by comparing the the value of the buttons and the correct value.
-            const correctBtn = buttons.find(button => button.dataset.value == rightButton)
-            // Adding this class on the rigth button when the user clicks the wrong button.
-            correctBtn.classList.add("true_answer");
-        }
+  // Function for setting the condition whether the user has answered or not yet for setting the condition whether the answer is correct or incorrect.
+  function handleClickButton(e) {
+    setAnswered(true)
+    if (e.target.dataset.value === data[item1].name) {
+      // If the condition is true, add this "true" class and set this setCorrect function into true.
+      e.target.classList.add('true_answer')
+      setCorrect(true)
+    } else if (e.target.dataset.value !== data[item1].name) {
+      // If this condition is true, add this "responded" class and set this setCorrect function into false.
+      e.target.classList.add('false_answer')
+      setCorrect(false)
+      // Finding the correct answer so that I can grab the button.
+      const rightButton =
+        data[
+          collectionItems.find((item) => {
+            return data[item].name === data[item1].name
+          })
+        ].name
+      // Grab all of the buttons in order to find the correct one.
+      const buttons = Array.from(document.querySelectorAll('.buttons'))
+      // Finding the button by comparing the the value of the buttons and the correct value.
+      const correctBtn = buttons.find(
+        (button) => button.dataset.value == rightButton
+      )
+      // Adding this class on the rigth button when the user clicks the wrong button.
+      correctBtn.classList.add('true_answer')
     }
+  }
 
-    return (
-        <>
-            <Displaycontent
-                correct={correct}
-                answered={answered}
-                setAnswered={setAnswered}
-                handleClickButton={handleClickButton}
-                ButtonToNext={ButtonToNext}
-                fetchingData={fetchingData}
-                count={count}
-                setCount={setCount}
-                data={data}
-                collectionItems={collectionItems}
-                item1={item1}
-            />
-        </>
-    )
+  return (
+    <>
+      <DisplayContent
+        correct={correct}
+        answered={answered}
+        setAnswered={setAnswered}
+        handleClickButton={handleClickButton}
+        ButtonToNext={ButtonToNext}
+        fetchingData={fetchingData}
+        count={count}
+        setCount={setCount}
+        data={data}
+        collectionItems={collectionItems}
+        item1={item1}
+      />
+    </>
+  )
 }
 
-export default Display;
-
+export default Display
--- component/Questions.js
+++ component/Questions.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import undraw_adventure from "../img/undraw_adventure.svg";
+import React from 'react'
+// import undraw_adventure from "../img/undraw_adventure.svg";
 
-function Questions({data, item1, fetchingData}) {
+function Questions({ data, item1, fetchingData }) {
+  // A variable that randomise the question displayed.
+  const MathRandomQuestions = Math.floor(Math.random() * 2)
+  if (!data[item1].capital || !data[item1].flag) {
+    fetchingData()
+  }
 
-    // A variable that randomise the question displayed.
-    const MathRandomQuestions = Math.floor(Math.random() * 2);
-    if ((!data[item1].capital) || (!data[item1].flag)) {
-        fetchingData()
-    }
-
-    return (
+  return (
+    <div>
+      <img
+        className='undraw_adventure'
+        src='../img/undraw_adventure.svg'
+        alt='this is an undraw adventure'
+      />
+      {MathRandomQuestions === 3 ? (
+        <h2 className='capitale_question'>
+          {data[item1].capital && data[item1].capital} is the capital of?
+        </h2>
+      ) : (
         <div>
-            <img className="undraw_adventure" src={undraw_adventure} alt="this is an undraw adventure" />
-            {MathRandomQuestions === 3 ? 
-                (<h2 className="capitale_question">{data[item1].capital && data[item1].capital} is the capital of?</h2>)
-                :
-                <div>
-                    <img src={data[item1].flag} className="flag" />
-                    <h2 className="flag_question">Which country does this flag belong to?</h2>
-                </div>
-            }
+          <img src={data[item1].flag} className='flag' />
+          <h2 className='flag_question'>
+            Which country does this flag belong to?
+          </h2>
         </div>
-    )
+      )}
+    </div>
+  )
 }
 
 export default Questions
